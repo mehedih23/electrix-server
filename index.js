9fa72f41e044459b0c044a5e52ddc057f34201aa
mehedih23 <nmehedi23@gmail.com>
2022-05-23 23:50:32 +0600

    P

--- index.js
+++ index.js
@@ -18,6 +18,21 @@ app.get('/', (req, res) => {
 })
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.JSONWEBTOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zsnpu.mongodb.net/?retryWrites=true&w=majority`;
@@ -37,7 +52,7 @@ async function run() {
         const userCollection = client.db("electrix").collection("users");
 
 
-        //-------------------- PAYMENT ------------------------//
+        //-------------------- PAYMENT START ------------------------//
         app.post("/create-payment-intent", async (req, res) => {
             const service = req.body;
             const price = service.price;
@@ -51,9 +66,9 @@ async function run() {
                 clientSecret: paymentIntent.client_secret,
             });
         });
+        //-------------------- PAYMENT End ------------------------//
 
-
-        // ------------------- ALL POST API ------------------- //
+        // ------------------- ALL POST API START ------------------- //
 
         // order //
         app.post('/order', async (req, res) => {
@@ -78,10 +93,10 @@ async function run() {
 
 
 
+        // ------------------- ALL POST API END ------------------- //
 
 
-
-        // ------------------- ALL GET API ------------------- //
+        // ------------------- ALL GET API START ------------------- //
 
         // Tools //
         app.get('/tools', async (req, res) => {
@@ -126,10 +141,11 @@ async function run() {
 
 
 
+        // ------------------- ALL GET API END ------------------- //
 
 
 
-        // ------------------- ALL PUT API ------------------- //
+        // ------------------- ALL PUT API START ------------------- //
 
         // User //
         app.put('/user/:email', async (req, res) => {
@@ -152,10 +168,11 @@ async function run() {
 
 
 
+        // ------------------- ALL PUT API END ------------------- //
 
 
 
-        // ------------------- ALL PATCH API ------------------- //
+        // ------------------- ALL PATCH API  START ------------------- //
         app.patch('/order/:id', async (req, res) => {
             const id = req.params.id;
             const payment = req.body;
@@ -176,11 +193,12 @@ async function run() {
 
 
 
+        // ------------------- ALL PATCH API END ------------------- //
 
 
 
 
-        // ------------------- ALL DELETE API ------------------- //
+        // ------------------- ALL DELETE API START ------------------- //
         // Order //
         app.delete('/order/:id', async (req, res) => {
             const id = req.params.id;
@@ -191,12 +209,7 @@ async function run() {
 
 
 
-
-
-
-
-
-
+        // ------------------- ALL DELETE API END ------------------- //
 
 
     }
@@ -206,24 +219,6 @@ async function run() {
 }
 
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(port, () => {
