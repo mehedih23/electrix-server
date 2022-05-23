fc75e8c184923d798395d6dd319f5110e9847e71
mehedih23 <nmehedi23@gmail.com>
2022-05-23 18:09:21 +0600

    payment api done

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express()
 const port = process.env.PORT || 1111
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-
-
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 
 
@@ -31,10 +30,27 @@ async function run() {
         const orderCollection = client.db("electrix").collection("orders");
 
 
+        //-------------------- PAYMENT ------------------------//
+        app.post("/create-payment-intent", async (req, res) => {
+            const service = req.body;
+            const price = service.price;
+            const amount = parseFloat(price) * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+
         // ------------------- ALL POST API ------------------- //
+
+        // order //
         app.post('/order', async (req, res) => {
             const order = req.body;
-            console.log(order)
             const result = await orderCollection.insertOne(order);
             res.send(result);
         })
@@ -69,6 +85,20 @@ async function run() {
         })
 
 
+        // Order //
+        app.get('/order', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const result = await orderCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.get('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.findOne(query);
+            res.send(result);
+        })
 
 
 
@@ -109,7 +139,13 @@ async function run() {
 
 
         // ------------------- ALL DELETE API ------------------- //
-
+        // Order //
+        app.delete('/order/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
 
