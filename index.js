94fee1c1b22b47f1aedc1e78a2135ea82adfe722
mehedih23 <nmehedi23@gmail.com>
2022-05-24 15:24:57 +0600

    user delete done

--- index.js
+++ index.js
@@ -140,6 +140,11 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/users', verifyJWT, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
 
 
 
@@ -217,6 +222,15 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+        // Users //
+        app.delete('/users/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
             res.send(result);
         })
 
